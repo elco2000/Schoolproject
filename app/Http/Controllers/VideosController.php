58e9f489a4e1d29fe007a63a9452b903a9924e1a
jeroen107 <jeroenlans107@gmail.com>
2018-05-22 10:14:44 +0200

    test

---
+++ app/Http/Controllers/VideosController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Video;
+
+class VideosController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $videos = video::all()->take(10);
+        return view ('video.index', compact('videos') );
+    }
+
+
+
+}
