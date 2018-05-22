d4033cffc9a4f83ddf9ba8c99551c34f36280248
jeroen107 <jeroenlans107@gmail.com>
2018-05-22 11:16:22 +0200

    test

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(VideosTableSeeder::class);
         $this->call(ChannelsTableSeeder::class);
     }
 }
