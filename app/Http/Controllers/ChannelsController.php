f99002bdfe6d5fb3e84847274ac60d47cccb8ee8
elco2000 <32056576+elco2000@users.noreply.github.com>
2018-05-23 13:51:14 +0200

    Correcte show

--- app/Http/Controllers/ChannelsController.php
+++ app/Http/Controllers/ChannelsController.php
@@ -59,6 +59,7 @@ class ChannelsController extends Controller
     public function show(Channel $channel)
     {
         //
+        return view('channels.show', compact('channel'));
     }
 
     /**
---
+++ resources/views/categories/show.blade.php
@@ -0,0 +1,6 @@
+<div class="blog-post">
+    <h2 class="blog-post-title">{{$category->name}}</h2>
+    <p class="blog-post-meta">April 4, 2018 by <a href="#">Stephan</a></p>
+
+    <p>{{$category->description}}</p>
+</div>
