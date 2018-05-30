c41ea22e8f453c75631ee2f1f1419ef7f808347e
SGordijn <stephangordijn@gmail.com>
2018-05-30 10:20:32 +0200

    Notificationtype

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(ReactionsTableSeeder::class);
+        $this->call(NotificationtypesTableSeeder::class);
     }
 }
