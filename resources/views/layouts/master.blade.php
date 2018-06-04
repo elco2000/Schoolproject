4fba7d7c84d5eb04faf283b272a839a62a763b41
elco2000 <32056576+elco2000@users.noreply.github.com>
2018-06-05 00:31:04 +0200

    Style

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -10,7 +10,7 @@
 <header class="blog-header py-3">
     <div class="row flex-nowrap justify-content-between align-items-center">
         <div class="col-4 pt-1">
-            <a class="text-muted" href="#">Laravel Project</a>
+            <a class="text-muted" href="{{url('/')}}">Home</a>
         </div>
         </div>
     </div>
