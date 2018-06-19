474044d0fa3b41dc64697779d1f322af405524ad
elco2000 <32056576+elco2000@users.noreply.github.com>
2018-06-19 17:28:33 +0200

    Merge branch 'development' of https://github.com/TechniekCollegeRotterdam/laravel-project-elco-jeroen-en-stephan into elco
    # Conflicts:
    #	app/Channel.php
    #	public/scss/styles.css.map

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -56,7 +56,7 @@
                             </div>
                         </div>
 
-                        
+
 
                         <div class="">
                             <div class="">
