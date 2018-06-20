7ec5ca82d3ab36ca5cdd71870bbfc10124b1b402
jeroen107 <jeroenlans107@gmail.com>
2018-06-20 09:04:45 +0200

    Merge branch 'development' of https://github.com/TechniekCollegeRotterdam/laravel-project-elco-jeroen-en-stephan into jeroen
    # Conflicts:
    #	app/Http/Controllers/VideosController.php

--- resources/views/channels/search.blade.php
+++ resources/views/channels/search.blade.php
@@ -1,12 +1,52 @@
 @extends('layouts.master')
 @section('content')
 
+
     <h1>Channels</h1>
+
+
 
     <form action="{{ route('channels.search') }}" method="POST" class="ajaxSearch">
         <input type="search" name="query" placeholder="Type something to search" autocomplete="off">
         <input type="submit" value="Search">
     </form>
+
+    <div class="dropdown">
+        <button onclick="myFunction()" class="dropbtn">Dropdown</button>
+        <div id="myDropdown" class="dropdown-content">
+            <input type="text" placeholder="Search.." id="myInput" onkeyup="filterFunction()">
+            <a href="{{URL::to('users')}}">Users</a>
+            <a href="{{URL::to('genres')}}">Genres</a>
+            <a href="{{URL::to('notificationtypes')}}">Notificationtypes</a>
+            <a href="{{URL::to('permissions')}}">Permissions</a>
+            <a href="{{URL::to('reactions')}}">Reactions</a>
+            <a href="{{URL::to('roles')}}">Role</a>
+            <a href="{{URL::to('videos')}}">Videos</a>
+        </div>
+    </div>
+
+    <script>
+        /* When the user clicks on the button,
+        toggle between hiding and showing the dropdown content */
+        function myFunction() {
+            document.getElementById("myDropdown").classList.toggle("show");
+        }
+
+        function filterFunction() {
+            var input, filter, ul, li, a, i;
+            input = document.getElementById("myInput");
+            filter = input.value.toUpperCase();
+            div = document.getElementById("myDropdown");
+            a = div.getElementsByTagName("a");
+            for (i = 0; i < a.length; i++) {
+                if (a[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
+                    a[i].style.display = "";
+                } else {
+                    a[i].style.display = "none";
+                }
+            }
+        }
+    </script>
 
 
 
@@ -24,6 +64,8 @@
 
 
 @endsection
+
+
 
     @section('scripts')
     <script>
